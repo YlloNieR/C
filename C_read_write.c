7d0fde22fc47246f954cdec1c138b936e0ba8b10
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2020-06-12 21:30:48 +0200

    read and write

--- C_read_write.c
+++ C_read_write.c
@@ -4,11 +4,24 @@
 int main() {
     FILE *ptr;
     char buffer[255];
-    ptr = fopen("C:/Users/Oliver Rein.DESKTOP-6TPP48V/Documents/GitHub/C/first.txt","r+");        // Lesen
-    // ptr = fopen("/home/morpheus/Tuts/C/first.txt","w+");     // Schreiben
-    // ptr = fopen("/home/morpheus/Tuts/C/first.txt","a+");
-    fscanf(ptr, "%s", buffer);
-    printf("ertses mal Lesen: %s",buffer);
+    //ptr = fopen("first.txt","r");        // Lesen
+    //ptr = fopen("first.txt","w+");     // Schreiben
+    ptr = fopen("first.txt","a+");
+    // fscanf(ptr, "%s", buffer);
+    // printf("ertses mal Lesen: %s\n",buffer);    // liest jedes Wort einzeln
+    // fgets(buffer, 255, ptr);                    // liest jeden String einzeln
+    // printf("ertses mal Lesen: %s\n",buffer);
+    
+    /*
+    char c;
+  
+    c= fgetc(ptr);
+    while(c != EOF){        // wenn nicht End Of File
+       printf("%c", c);
+        c = fgetc(ptr);        
+    }
+    */
+   fprintf(ptr, "2.irgendein Text\n");
 
     return 0;
 }
